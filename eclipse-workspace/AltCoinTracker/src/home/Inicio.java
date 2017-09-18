8cb8c434ab0b33c5329736b14f515719ef126a3d
Juan2 <juan.alvarez.vaa@gmail.com>
2017-09-18 14:30:29 -0300

    asdasd

--- eclipse-workspace/AltCoinTracker/src/home/Inicio.java
+++ eclipse-workspace/AltCoinTracker/src/home/Inicio.java
@@ -7,7 +7,6 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 
 import com.tictactec.ta.lib.Core;
